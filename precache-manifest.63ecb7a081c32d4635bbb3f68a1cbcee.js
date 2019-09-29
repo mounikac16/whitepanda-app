2cdcfaebe529fe13c3a03cc55c504fc370075af4
chadalavada mounika <mounika.c16@iiits.in>
2019-09-29 17:49:39 +0530

    Updates

---
+++ precache-manifest.63ecb7a081c32d4635bbb3f68a1cbcee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c864574c96b7194dadd8ac669a0fe368",
+    "url": "/whitepanda-app/index.html"
+  },
+  {
+    "revision": "ef435a207386d41adc43",
+    "url": "/whitepanda-app/static/css/main.65b5d976.chunk.css"
+  },
+  {
+    "revision": "322ef6e2c791ee801add",
+    "url": "/whitepanda-app/static/js/2.eb69d9ed.chunk.js"
+  },
+  {
+    "revision": "ef435a207386d41adc43",
+    "url": "/whitepanda-app/static/js/main.13d24788.chunk.js"
+  },
+  {
+    "revision": "ec785d4ac388664de5d9",
+    "url": "/whitepanda-app/static/js/runtime-main.d6967d7d.js"
+  },
+  {
+    "revision": "679e529cd0097cd762fe845786ab4ae1",
+    "url": "/whitepanda-app/static/media/logo.679e529c.JPG"
+  }
+]);
